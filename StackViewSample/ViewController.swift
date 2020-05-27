1dbaa4aef68ae2ff7cf10b08ccd3c92d7d028a15
yu sato <yu_sato@yumemi.co.jp>
2020-05-27 17:10:02 +0900

    first commit

---
+++ StackViewSample/ViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var stackView: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
